8a3c73c10410fb9f839fb2aaa24ccf1555a7a10b
joaqock <joaqantonino@gmail.com>
2025-12-20 18:31:24 -0300

    fixes - about us page

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -23,7 +23,7 @@ function Nosotros() {
           </p>
         </div>
 
-        <div className="mt-10 bg-gray-900 border border-gray-800 rounded-2xl shadow-[0_0_30px_rgba(249,115,22,0.6)] p-8">
+        <div className="mt-10 bg-gray-900 border border-gray-800 rounded-2xl shadow-[0_0_30px_rgba(249,115,22,0.5)] p-8">
           <h3 className="text-2xl font-bold text-gray-300 mb-6">
             ¿Qué nos define?
           </h3>
