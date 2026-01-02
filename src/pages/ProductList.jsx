fa012d7223e33fe4afb68917b1bc2c6b24501696
joaqock <joaqantonino@gmail.com>
2026-01-02 14:43:10 -0300

    feat: add product images using cloudinary urls

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -27,12 +27,13 @@ return (
   <section className="w-full bg-gray-950 px-6 py-12">
     <h2 className="text-2xl font-semibold mb-6 py-6 text-orange-500">Productos</h2>
 
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
       {products.map(product => (
         <div
           key={product.id}
-          className="bg-gray-800 p-4 rounded-lg shadow"
+          className="bg-gray-800 p-1 rounded-lg shadow"
         >
+          <img className="object-cover h-64 w-full" src={product.imageUrl} alt={product.name}/>
           <p className="text-gray-100 font-medium">{product.name}</p>
           <p className="text-gray-100">${product.price}</p>
         </div>
