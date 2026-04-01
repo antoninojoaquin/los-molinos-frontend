184fef46496e87da1b9934ae2f44ce73ef084fdc
Joaquín Antonino <joaqantonino@gmail.com>
2026-04-01 19:20:15 -0300

    fix: change rounded of CTA button on hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
           Venta de accesorios para tu <span className="text-orange-500">parrilla</span> en Dolores y la zona
         </p>
         <a href="/productos" className="cursor-pointer flex justify-center ">
-          <button className="flex items-center gap-4 cursor-pointer text-2xl mt-6 px-4 py-2 border-2 shadow-[0_0_20px_rgba(0,0,0,1)] bg-orange-500 border-orange-500 text-white font-medium rounded-xl hover:text-white hover:bg-orange-400 hover:border-orange-400 transition-all duration-500">
+          <button className="flex items-center gap-4 cursor-pointer text-2xl mt-6 px-4 py-2.5 border-2 shadow-[0_0_20px_rgba(0,0,0,1)] bg-orange-500 border-orange-500 text-white font-medium rounded-full hover:text-white hover:bg-orange-400 hover:border-orange-400 transition-all duration-500">
             <FaCircleArrowRight className="text-4xl"/>
             Explorar Productos
           </button>
